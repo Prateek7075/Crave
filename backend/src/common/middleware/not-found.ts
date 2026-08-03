7a9156453516db8ed6a152290161cb606e56f351
Prateek <yadavprateek915@gmail.com>
2026-08-03 22:47:09 +0530

    chore: initialize Laravel backend foundation

--- backend/src/common/middleware/not-found.ts
+++
@@ -1,7 +0,0 @@
-import type { NextFunction, Request, Response } from "express";
-
-import { AppError } from "../errors/app-error.js";
-
-export function notFoundHandler(_request: Request, _response: Response, next: NextFunction): void {
-  next(new AppError("The requested API route does not exist", 404, "ROUTE_NOT_FOUND"));
-}
